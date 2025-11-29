5c594f2ae21d191e9ce9c554335966b5e24b3d66
aryadnnagm2 <aryadnnagm@gmail.com>
2025-11-28 23:19:30 -0300

    Create page.js

---
+++ app/register/page.js
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Registro</h1>
+      <p>No es necesario registrarse manualmente.  
+      Simplemente iniciá sesión con tu email.</p>
+    </div>
+  );
+}
